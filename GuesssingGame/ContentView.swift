63a53e6e16cfd00382f74d87eaaee87076f7381b
Matt Collyer <mcollyer@lcs.on.ca>
2021-11-30 14:55:37 -0500

    Fixing accidentally putting GuessingGame in a already created project and making it its own.

--- GuesssingGame/ContentView.swift
+++ GuesssingGame/ContentView.swift
@@ -1,21 +1,69 @@
 //
-//  ContentView.swift
-//  GuesssingGame
+//  GuessingGame.swift
+//  StatePropertyWrapperSimpleExample
 //
 //  Created by Matthew Collyer on 2021-11-30.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct GuessingGame: View {
+    
+    // creat the user is guessing
+    @State var currentGuess = 50.0
+    // Generate random number
+    let target = Int.random(in: 1...100)
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            
+            HStack{
+              Spacer()
+                Text("\( String(format: "%.0f", currentGuess)) ")
+                    .font(.title)
+                    .bold
+             
+            }
+            
+            
+            
+            
+            Slider(value: $currentGuess, in: 0.0...100.0, label: {
+                Text("Guess")
+            },
+                   minimumValueLabel: {
+                Text("0.0")
+            },
+                   
+                   maximumValueLabel: {
+                Text("100.0")
+            })
+            Button(action: {
+                print("Guess Submitted")
+            }, label: {
+                Text("Submit Guess")
+            })
+                .buttonStyle(.bordered)
+            
+            Text("The actaul secret number is: \(target)")
+            
+            Spacer()
+            
+        }
+        .padding()
+        .navigationTitle("Guessing Game")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+
+
+struct GuessingGame_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView{
+            GuessingGame()
+                .preferredColorScheme(.dark)
+        }
     }
 }
