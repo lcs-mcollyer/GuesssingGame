e7c5fa99e2388b7c6a2b8effa960dda058904024
Matt Collyer <mcollyer@lcs.on.ca>
2021-11-30 14:54:06 -0500

    Initial Commit

---
+++ GuesssingGame/GuesssingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuesssingGameApp.swift
+//  GuesssingGame
+//
+//  Created by Matthew Collyer on 2021-11-30.
+//
+
+import SwiftUI
+
+@main
+struct GuesssingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
